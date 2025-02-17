b55e3b52df594726e0281f8909521373eb0ca9e3
hungdq41 <hungdq41@fpt.com>
2025-02-17 15:01:50 +0700

    REmove redundant

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg'
-import './App.css'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
 import About from './components/About'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import NavBar from './components/NavBar'
 import Services from './components/Services'
 import Tours from './components/Tours'
-import Footer from './components/Footer'
+
 function App() {
   return (
     <>

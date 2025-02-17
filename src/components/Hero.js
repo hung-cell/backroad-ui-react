ad43d88c2c5cf6e0a04009b34ae8709ab9c4b58b
hungdq41 <hungdq41@fpt.com>
2025-02-17 14:53:28 +0700

    fix build

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,24 @@
 const Hero = () => {
+  const scrollToTours = (e) => {
+    e.preventDefault()
+    const tours = document.getElementById('tours')
+    tours?.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
     <section className='hero' id='home'>
       <div className='hero-banner'>
-        <h1>Backroads App</h1>
+        <h1>continue exploring</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
           explicabo debitis est autem dicta.
         </p>
-        <a href='#tours' className='btn hero-btn'>
+        <button className='btn hero-btn' onClick={scrollToTours}>
           explore tours
-        </a>
+        </button>
       </div>
     </section>
   )
 }
+
 export default Hero
